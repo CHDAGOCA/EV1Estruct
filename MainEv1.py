996e23452f362a9716361fbf435224977426bd5a
Who-is-Mike <126839598+Who-is-Mike@users.noreply.github.com>
2023-03-03 16:22:27 -0600

    Update MainEv1.py
    Por el momento solamente hice cambios esteticos en el codigo

--- MainEv1.py
+++ MainEv1.py
@@ -3,22 +3,22 @@ while True:
     menu_principal=input("Bienvenido a la biblioteca universitaria\n ¿Que accion deseas realizar? \n[1]Registrar un nuevo ejemplar \n[2]Consultas y reportes \n[3]Salir \n")
     if menu_principal== "1":
         while True:
-                titulo=input("\nTitulo del libro ")
+                titulo=input("\nTitulo del libro: ")
                 if titulo.strip() == '': 
                     break
                 autor=input(f"Ingrese el autor de {titulo} ")
                 if autor.strip() == '':
                     break
-                genero=input(f"Ingrese el genero al que pertenece ")  
+                genero=input(f"Ingrese el genero al que pertenece: ")  
                 if genero.strip() == '':
                     genero=input(f"El genero del libro es un dato obligatorio ") 
-                publicacion=input("Ingrese la fecha de publicación")
+                publicacion=input("Ingrese la fecha de publicación: ")
                 if publicacion.strip() == '':
                     publicacion=input(f"La fecha de publicacion es un campo obligatorio ")
-                isbn=input("Agregue el ISBN del libro a registrar")
+                isbn=input("Agregue el ISBN del libro a registrar: ")
                 if isbn.strip() == '':
                     isbn=input(f"El ISBN del libro es un dato obligatorio ")
-                fecha_adquisicion=input("Ingrese la fecha en la que se adquirio el libro")
+                fecha_adquisicion=input("Ingrese la fecha en la que se adquirio el libro: ")
                 if fecha_adquisicion.strip() == '':
                     fecha_adquisicion=input(f"La fecha en que se adquirio el libro es un dato obligatorio ")
 
