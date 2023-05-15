12f65b1c6a21ffd55a1b91bf4520af8ce6c52663
CHDAGOCA <126837718+CHDAGOCA@users.noreply.github.com>
2023-05-15 15:18:23 -0600

    Update EV3_Ver3
    arreglo de formato.

--- MainEv1.py
+++ MainEv1.py
@@ -483,11 +483,11 @@ def ExportArchAñoPublic_Excel(añosearch):
     print("El reporte ", archname ," fue creado exitosamente y esta en ",ruta)
 
 
-def GuardarLibros(id,titulo,autor,genero,añopub,isbn,fechadqdia,fechadqmes,fechañodq):
+def GuardarLibros(titulo,autor,genero,añopub,isbn,fechadqdia,fechadqmes,fechañodq):
     try:
         with sqlite3.connect("Biblioteca.db") as conn:
             mi_cursor=conn.cursor()
-            valores = (titulo,autor,genero,datetime.datetime(añopub,1,1),isbn,datetime.datetime(fechadqdia,fechadqmes,fechañodq))
+            valores = (titulo,autor,genero,datetime.datetime(añopub,1,1),isbn,datetime.datetime(fechañodq,fechadqmes,fechadqdia))
             mi_cursor.execute("INSERT INTO Libros (titulo,autor,genero,añopublicacion,ISBN,fechaadq) VALUES(?,?,?,?,?,?)", valores)
         print("Registros agregado exitosamente.")
     except Error as e:
